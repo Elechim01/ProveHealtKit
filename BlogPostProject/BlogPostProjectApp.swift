7de5e68769f847bdd0feab3bc77054b4005b05c7
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-07 23:21:23 +0200

    Initial Commit

---
+++ BlogPostProject/BlogPostProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogPostProjectApp.swift
+//  BlogPostProject
+//
+//  Created by Michele Manniello on 07/08/21.
+//
+
+import SwiftUI
+
+@main
+struct BlogPostProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
